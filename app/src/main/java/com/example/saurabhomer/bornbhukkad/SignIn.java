8d247d962829935f733e7b35f10d3a4fac515956
saurabhomer258 <saurabhomer258@gmail.com>
2018-04-02 09:06:02 -0700

    login page

--- app/src/main/java/com/example/saurabhomer/bornbhukkad/SignIn.java
+++ app/src/main/java/com/example/saurabhomer/bornbhukkad/SignIn.java
@@ -18,17 +18,20 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.rengwuxian.materialedittext.MaterialEditText;
+
+import info.hoang8f.widget.FButton;
+
 public class SignIn extends AppCompatActivity {
 
     EditText edtPhone,edtPassword;
-    Button btnSignIn;
+    FButton btnSignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-        edtPassword =(MaterialEditText)findViewById(R.id.edtPassword);
-        edtPhone =(MaterialEditText)findViewById(R.id.edtPhone);
-        btnSignIn =(Button) findViewById(R.id.btnSignIn);
+        edtPassword =(EditText)findViewById(R.id.edtPassword);
+        edtPhone =(EditText)findViewById(R.id.edtPhone);
+        btnSignIn =(FButton) findViewById(R.id.btnSignIn);
 
 
         //Init Firebase
